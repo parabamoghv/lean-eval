5eb81dd217cb25e622c24c5b02755c84df3b0a1a
Li Xuanji <xuanji@gmail.com>
2026-05-03 05:54:47 -0400

    feat(NumberTheory): Add FLT (#83)
    * feat(NumberTheory): Add FLT
    
    * add lean

---
+++ LeanEval/NumberTheory/FermatLastTheorem.lean
@@ -0,0 +1,13 @@
+import Mathlib.NumberTheory.FLT.Basic
+import EvalTools.Markers
+
+namespace LeanEval
+namespace NumberTheory
+
+@[eval_problem]
+theorem fermat_last_theorem :
+    FermatLastTheorem := by
+  sorry
+
+end NumberTheory
+end LeanEval
